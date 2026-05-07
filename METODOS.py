a95f7eceb1cb1676b07d059f1a78576f4f03a205
GABRIELPINTODASILVA72 <gabrielpintodasilva72@gmail.com>
2026-05-07 20:57:41 -0300

    PARA TESTAR

---
+++ METODOS.py
@@ -0,0 +1,23 @@
+#def adicao(a,b):
+#    soma = a + b
+#
+#    return soma
+#a = int(input("a"))
+#b = int(input("b"))
+#print(adicao(a,b))
+
+# raiz quadrada  
+
+def nada(a,b = 2):
+
+    raiz = a ** (1/b)
+
+
+
+    return raiz
+
+a = int(input("num da base"))
+b = int(input("num elevado"))
+
+print(nada(a,b ))
+#kjkasdjfçlkasdjf
