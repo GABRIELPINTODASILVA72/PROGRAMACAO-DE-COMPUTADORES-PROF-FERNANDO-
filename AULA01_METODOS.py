083fdaab34e57857fbd643dea5e196bd2e2202e6
GABRIELPINTODASILVA72 <gabrielpintodasilva72@gmail.com>
2026-05-07 21:45:10 -0300

    aulakk

---
+++ AULA01_METODOS.py
@@ -0,0 +1,57 @@
+#def adicao(a,b):
+#    soma = a + b
+#
+#    return soma
+#a = int(input("a"))
+#b = int(input("b"))
+#print(adicao(a,b))
+
+# raiz quadrada  
+
+#def nada(a,b = 2):
+
+#    raiz = a ** (1/b)
+
+
+
+#    return raiz
+
+#a = int(input("num da base"))
+#b = int(input("num elevado"))
+
+#print(nada(a,b ))
+
+# criar uma função que retorne se o num passado 
+# "e par " ou "e impar"
+
+#def nada(n):
+#    if n % 2 == 0:
+#        return "e par"
+#    else:
+#        return "e impar"
+    
+#print(nada(2))
+#print(nada(3))   
+ 
+# ou 
+
+#def nada(n):
+#    return n % 2 == 0 and "e par " or "e impar"
+    
+    
+# print(nada(2))
+# print(nada(3)) 
+
+# aula de lista mais uma forma de eliminar for e wile lops
+
+# list = [0, 35.2 , 23.0 , 37.3, 36.0 , -28] #  temperatura °c
+# print(list(map(lambda c : (9/5) * c + 32 )))
+# criar uma nova lista em °f
+# F = (9/5) * C + 32
+
+
+list = [4, 2, 5, 6, 2, 1, 6]
+
+
+
+
